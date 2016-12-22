f735e4b42d132c8132ba4464f8b0e045faaa77ad
Matthew McGonagle <mwmcgonagle@gmail.com>
2016-12-22 01:40:18 -0500

    Removed old HilbertTree implementations. Now HilbertTree uses the symmetry class to keep track of orientation. Positions for squares are simply the lower left corners.
    Positions are now pulled from tree using a maximum function that uses
    position, a width, and a height.
    
    Implemented a maximum function for random circles, a particular
    function, and also for drawing a black and white image stored as a 2d
    array. The image implmentation could use some fine tuning and maybe a
    better algorithm for deciding what level to choose.
    
    Process could be optimized. Right now, entire tree is built, but this is
    unnecessary. Also, instead of computing max func, could compute a "do we
    continue" function. This could short circuit and save compuation time.

--- HilbertDraw.py
+++ HilbertDraw.py
@@ -1,29 +1,20 @@
 import numpy as np
 
-LOWLEFTSTART = 0
-UPLEFTSTART = 1
-UPRIGHTSTART = 2
-LOWRIGHTSTART = 3
-
-CLOCKWISE = 1
-COUNTERCLOCKWISE = -1
-
 class SquareSymmetry:
 
-	# reflection rotation^k
+	# rotation^k * reflection
 
-	def __init__(self, reflection, rotation):
+	def __init__(self, rotation, reflection):
 		self.rotation = rotation % 4
 		self.reflection = reflection % 2
 
 	def times(self, symmetry):
-		newrotation = self.rotation
-		newreflection = self.reflection
-		if symmetry.reflection:
+		newrotation = symmetry.rotation
+		if self.reflection:
 			newrotation = (4 - newrotation) % 4
-			newreflection = (newreflection + symmetry.reflection) % 2
-		newrotation = (newrotation + symmetry.rotation) % 4
-		return SquareSymmetry(newreflection, newrotation)
+		newreflection = (self.reflection + symmetry.reflection) % 2
+		newrotation = (newrotation + self.rotation) % 4
+		return SquareSymmetry(newrotation, newreflection)
 
 	def getreflection(self):
 		return self.reflection
@@ -42,10 +33,10 @@ class SquareSymmetry:
 	def actindex(self, index):
 		newindex = (self.rotation + index) % 4
 		if self.reflection > 0:
-			newindex = (3*index) % 4
+			newindex = (3*(newindex+1)) % 4
 		return newindex
 
-class HilbertSquare3:
+class HilbertTree:
 
 	def __init__(self, symmetry, level):
 		self.symmetry = symmetry
@@ -59,18 +50,19 @@ class HilbertSquare3:
 			return
 
 		newlevel = self.level + 1
-		newsymmetry = self.symmetry.times(SquareSymmetry(1,1))
-		self.children.append(HilbertSquare3(newsymmetry, newlevel))
+		newsymmetry = self.symmetry.times(SquareSymmetry(3,1))
+		self.children.append(HilbertTree(newsymmetry, newlevel))
 
 		newsymmetry = self.symmetry
-		self.children.append(HilbertSquare3(newsymmetry, newlevel))
-		self.children.append(HilbertSquare3(newsymmetry, newlevel))
+		self.children.append(HilbertTree(newsymmetry, newlevel))
+		self.children.append(HilbertTree(newsymmetry, newlevel))
 
-		newsymmetry = self.symmetry.times(SquareSymmetry(1,3))
-		self.children.append(HilbertSquare3(newsymmetry, newlevel))
+		newsymmetry = self.symmetry.times(SquareSymmetry(1,1))
+		self.children.append(HilbertTree(newsymmetry, newlevel))
 
 		for i in range(4):
 			self.children[i].generatechildren(numlevels)
+
 	def generatepositions(self, currentlist, myposition, mywidth):
 		if not self.children:
 			currentlist.append(myposition)
@@ -84,318 +76,17 @@ class HilbertSquare3:
 				newposition[j] += offsets[newi][j]
 			self.children[i].generatepositions(currentlist, newposition, newwidth) 
 
-
-class HilbertSquare2:
-
-	def __init__(self, symmetry, position, logwidth, level, backwards):
-		self.symmetry = symmetry
-		self.position = position.copy()
-		self.logwidth = logwidth
-		self.level = level
-		self.backwards = backwards 
-		self.children = []
-
-	def addoffset(self, offset):
-		newposition = [0,0]
-		for i in range(2):
-			newposition[i] = self.position[i]+offset[i]
-		return newposition
-
-	def generatechildren(self, level):
-		self.children = []
-		if level > 0:
-			newlog = self.logwidth - 1
-			newwidth = 2**newlog
-			width = 2**self.logwidth
-			newlevel = level - 1
-			newbackwards = self.backwards
-
-			newsymmetry = self.symmetry.times(SquareSymmetry(1,1))
-			offset = [0,0]
-			newposition = self.addoffset(offset)
-			self.children.append(HilbertSquare2(newsymmetry, newposition, newlog, newlevel, newbackwards))
-
-			newsymmetry = self.symmetry
-			offset = [0, newwidth]
-			offset = self.symmetry.action(offset)
-			newposition = self.addoffset(offset)
-			self.children.append(HilbertSquare2(newsymmetry, newposition, newlog, newlevel, newbackwards))
-
-			newsymmetry = self.symmetry#.times(SquareSymmetry(1,0))
-			offset = [newwidth, newwidth]
-			offset = self.symmetry.action(offset)
-			newposition = self.addoffset(offset)
-			#newbackwards = (self.backwards + 1) % 2
-			self.children.append(HilbertSquare2(newsymmetry, newposition, newlog, newlevel, newbackwards))
-
-			newsymmetry = self.symmetry.times(SquareSymmetry(1,3))
-			offset = [width, newwidth]
-			offset = self.symmetry.action(offset)
-			newposition = self.addoffset(offset)
-			self.children.append(HilbertSquare2(newsymmetry, newposition, newlog, newlevel, newbackwards))
-
-			for i in range(len(self.children)):
-				self.children[i].generatechildren(newlevel)
-
-			
-	def getpositions(self, currentpositions):
-		if not self.children:
-			width = 2**(self.logwidth)
-			newwidth = 2**(self.logwidth-1)
-			#height = 2**(self.logwidth)
-			#spacing = 2**(self.logwidth-2) 
-			#offsets = [[spacing,spacing], [spacing, height-spacing], [width-spacing,height-spacing], [width-spacing,spacing] ]
-			#offsets = [[0,0], [0, height-spacing], [width-spacing,height-spacing], [width-spacing,0] ]
-			#numoffsets = len(offsets)
-			#if not self.backwards:
-			#	for i in range(numoffsets):
-			#		newposition = self.addoffset(self.symmetry.action(offsets[i]))
-			#		currentpositions.append(newposition)
-			#else:
-			#	for i in range(numoffsets):
-			#		newposition = self.addoffset(self.symmetry.action(offsets[numoffsets-1-i]))
-			#		currentpositions.append(newposition)
-		#else:
-			#if not self.backwards:
-			#	for i in range(4):
-			#		self.children[i].getpositions(currentpositions)
-			#else:
-			#	for i in range(4):
-			#		self.children[3-i].getpositions(currentpositions)
-			offsets = [[0,0], [0,newwidth], [newwidth, newwidth], [newwidth, 0]] 
-			for i in range(len(offsets)):
-				newposition = self.symmetry.action(offsets[i])
-				newposition = self.addoffset(newposition)
-				currentpositions.append(newposition)
-		else:
-			for i in range(4):
-				self.children[i].getpositions(currentpositions)	
-		
-class HilbertConfig:
-
-	def __init__( self, start, orientation, position, drawposition, logwidth):
-		self.start = start
-		self.orientation = orientation
-		self.position = position
-		self.drawposition = drawposition
-		self.logwidth = logwidth
-
-	def gettransformed(self, fourarray, index):
-		if self.orientation == COUNTERCLOCKWISE:
-			newindex = (index + self.start) % 4
-		else:
-			newindex = (4 - index + self.start) % 4
-		return fourarray[newindex]
-
-	def getoffsets(self, size):
-		return [ [0,0], [0, size], [size, size], [size,0] ]
-
-
-
-	def subconfig(self, index):
-		index = index % 4
-		if index == 0:
-			newstart = self.start
-			neworientation = self.orientation * -1
-		elif index == 1 or index == 2:
-			newstart = self.start
-			neworientation = self.orientation 
-		else:
-			newstart = (self.start + 2) % 4
-			neworientation = self.orientation * -1 
-		newwidth = 2**(self.logwidth-1)
-		newoffsets = self.getoffsets(newwidth)
-		#newoffsets = [ [0,0], [0, newwidth], [newwidth, newwidth], [newwidth,0] ]
-		#if self.orientation == COUNTERCLOCKWISE:
-		#	newposition = newoffsets[(self.start + index) % 4]
-		#else:
-		#	newposition = newoffsets[(self.start + 4 - index) % 4]
-		newposition = self.gettransformed(newoffsets, index)
-		for i in range(2):
-			newposition[i] += self.position[i]
-
-		newheight = newwidth
-		newwidth = 2**(self.logwidth)-1
-		newoffsets = self.getoffsets(newwidth)
-		newoffsets = [[0,0], [0, newheight], [newwidth, newheight], [newwidth, 0]]
-		newdrawposition = self.gettransformed(newoffsets, index)
-		for i in range(2):
-			newdrawposition[i] += self.position[i]
-
-		
-		
-		return HilbertConfig(newstart, neworientation, newposition, newdrawposition, self.logwidth - 1)
-
-class HilbertSquare:
-
-	def __init__( self, parent, config):
-		self.parent = parent
-		self.config = config
-		self.children = []
-		self.floor = 0
-
-	def generatechildren(self, level):
-		if level > 0:
-			for i in range(4):
-				newconfig = self.config.subconfig(i)
-				self.children.append(HilbertSquare(self, newconfig))
-				self.children[i].generatechildren(level-1)
-
-	def generatewithmin(self, level, maxfunc):
-		self.floor = maxfunc(self.config.position, self.config.logwidth)
-		if level > 0 and level > self.floor:
-			for i in range(4):
-				newconfig = self.config.subconfig(i)
-				self.children.append(HilbertSquare(self, newconfig))
-				self.children[i].generatewithmin(level-1, maxfunc)
-
-
-	def getpositions(self, currentpositions):
-		if not self.children:
-			currentpositions.append(self.config.drawposition)
-		else:
-			for i in range(4):
-				self.children[i].getpositions(currentpositions)
-
-	def getpositionswithmin(self, currentpositions):
-		if not self.children and self.floor < self.config.logwidth:
-			currentpositions.append(self.config.position)
-		else:
-			level = self.config.logwidth
-			offset = 2**(level-1)
-			offset = 2**level - 1
-			centers = [[offset-1, offset-1],
-				   [offset-1, offset],
-				   [offset,offset],
-				   [offset,offset-1]]
-			for i in range(4):
-				if self.children[i].floor < level:
-					self.children[i].getpositions(currentpositions)
-				else:
-					print("Jump")
-
-				#if self.children[i].floor > 2: #level - 4:
-				#	print("Jump")
-				#	newposition = centers[(self.config.orientation+i)%4]
-				#	for k in range(2):
-				#		newposition[k] += self.config.position[k]
-				#	currentpositions.append(newposition.copy())
-				#else:
-				#	self.children[i].getpositions(currentpositions)
-
-class FourSquares:
-
-	def __init__(self, parent, start, orientation):
-		self.parent = parent
-		self.start = start
-		self.orientation = orientation
-		self.children = []
-
-	def generatechildren(self):
-		newstart = self.start 
-		neworientation = self.orientation * -1
-		self.children.append( FourSquares(self, newstart, neworientation) )
-
-		neworientation *=  -1
-		self.children.append( FourSquares(self, newstart, neworientation) )
-		self.children.append( FourSquares(self, newstart, neworientation) )
-
-		newstart = (newstart + 2) % 4
-		neworientation *= -1 
-		self.children.append( FourSquares(self, newstart, neworientation) )
-
-def generatesquaretree(currentsquare, level):
-	if(level < 1):
-		return
-	currentsquare.generatechildren()
-	for i in range(4):
-		generatesquaretree(currentsquare.children[i], level-1)
-
-def printsquaretree(currentsquare, level):
-	print(level, end="")
-	for i in range(level):
-		print(". . . . . . ", end = "")
-	print("Start = ", currentsquare.start, end = "")
-	print(", Orientation = ", currentsquare.orientation)
-	if not currentsquare.children:
-		return
-	else:
+	def generatepositionswithmax(self, currentlist, myposition, mywidth, myheight, maxfunc):
+		if (not self.children) or self.level > maxfunc(myposition, mywidth, myheight):  
+			currentlist.append(myposition)
+			return
+		newwidth = mywidth/2
+		newheight = myheight/2
+		offsets = [[0,0], [0,newheight], [newwidth,newheight], [newwidth,0]]
 		for i in range(4):
-			printsquaretree(currentsquare.children[i], level+1)
-		return
+			newi = self.symmetry.actindex(i)
+			newposition = myposition.copy()
+			for j in range(2):
+				newposition[j] += offsets[newi][j]
+			self.children[i].generatepositionswithmax(currentlist, newposition, newwidth, newheight, maxfunc) 
 
-def generatepositions(poslist, currentpos, currentsquare, level, onlyint = False):
-	if not currentsquare.children: 
-		poslist.append(currentpos)
-		return
-
-	if level<0 and onlyint:
-		print("Error generatepositions: level = ", level)
-		return
-
-	offset = 2**level
-	if(currentsquare.orientation == COUNTERCLOCKWISE):
-		outoforderoffset = [[0, 0],
-				 [0, offset],
-				 [offset, offset],
-				 [offset, 0]]
-		newoffset = [outoforderoffset[(i+currentsquare.start)%4] for i in range(4)]
-	else:
-		outoforderoffset = [[0, 0],
-				 [offset, 0],
-				 [offset, offset],
-				 [0, offset]]
-		newoffset = [outoforderoffset[(i+currentsquare.start)%4] for i in range(4)]
-	
-	for i in range(4):
-		newpos = currentpos.copy()
-		for j in range(2):
-	        	newpos[j] = newoffset[i][j] + currentpos[j]		
-		generatepositions(poslist, newpos, currentsquare.children[i], level-1)
-
-	return
-
-def paintpositions(coord, pos1, pos2):
-	if pos1[0] != pos2[0]:
-		j = pos1[1]
-		change = pos2[0] - pos1[0]
-		if change > 0: 
-			start = pos1[0]
-		else:
-			start = pos2[0]
-			change *= -1
-		for i in range(change+1):
-			k = start + i 
-			coord[k][j] = "*" 
-	if pos1[1] != pos2[1]:
-		i = pos1[0]
-		change = pos2[1] - pos1[1]
-		if change > 0: 
-			start = pos1[1]
-		else:
-			start = pos2[1]
-			change *= -1
-		for j in range(change+1):
-			k = start + j 
-			coord[i][k] = "*" 
-
-	return
-		
-
-def HilbertDraw( coord, level):
-	size = len(coord)
-	if( len(coord[0]) != size ):
-		print("Error HilbertDraw: coord not square array.")
-		return
-
-	power = np.log2(size)
-	if( int(power) != power):
-		print("Error HilbertDraw: dimensions coord are not integer power of two.")
-		return
-	power = int(power)
-	
-	if(level > power):
-		print("Error HilbertDraw: level > power")
-		return
-
-	
--- main.py
+++ main.py
@@ -2,105 +2,105 @@ import HilbertDraw as hd
 import matplotlib.pyplot as plt
 import pylab
 import numpy as np
+import scipy
 import random 
-
-power = 7
-coord = [[power+1 for j in range(2**power)] for i in range(2**power)] 
-
-def paintcircle(center, radius, value):
-	for i in range(2**power):
-		for j in range(2**power):
-			distance = (center[0]-i)**2 + (center[1] - j)**2
-			distance = np.sqrt(distance)
-			if distance < radius:
-				coord[i][j] = value
-#for i in range(2**power):
-#	for j in range(2**power):
-#		coord[i][j] = i+j
-#		coord[i][j] *= 2**(-power-1)
-#		coord[i][j] = 1 - coord[i][j]
-#		coord[i][j] *= power
-#		coord[i][j] = np.sin(i*2**(-power)*9*np.pi)
-#		coord[i][j] *= np.sin(j*2**(-power)*5*np.pi)
-#		coord[i][j] += 1
-#		coord[i][j] *= 1/2
-#		coord[i][j] *= i*(2**(power)-i)*2**(-power+1)*(2**(power)-2**(power-1))**-1
-#		coord[i][j] *= power 
-
-paintcircle([0,0], 1.5*2**(power-1), 0)
-paintcircle([2**(power-1), 2**(power-1)], 2**(power-2)-1, 2)
-paintcircle([2**(power),2**(power)], 2**(power-1), 1)
-
-#for i in range(20):
-#	centerx =  int(2**power*random.random())
-#	centery = int(2**power*random.random())
-#	radius = int(2**(power-2)*random.random())
-#	value = 2* random.random() 
-#	paintcircle([centerx, centery], radius, value)
-
-squarelevels = 7
-def floorfunc(position, logwidth):
-	offset = 2**logwidth
-	result = position[0]**2 + position[1]**2
-	result *= 2**(-2*squarelevels-0) 
-	result *= squarelevels
-	result -= 1
-	return result
-
-def floorfunc2(position, logwidth):
-	offset = int(2**logwidth)
-	i0 = int(position[0])
-	j0 = int(position[1])
-	minimum = squarelevels 
-	for i in range(i0, i0+offset):
-		for j in range(j0, j0+offset):
-			if coord[i][j] < minimum:
-				minimum = coord[i][j]
-	return minimum
+from PIL import Image
+import PIL.ImageOps
 
 initsymmetry = hd.SquareSymmetry(0,0)
 initlevel = 0
-numlevels = 1
+numlevels = 7
+treewidth = 2**numlevels
+
+circles = []
+for i in range(15):
+	radius = random.random()*treewidth/3
+	xcenter = random.random()*treewidth
+	ycenter = random.random()*treewidth
+	weight = numlevels - 3 + random.random()*4
+	circles.append([xcenter, ycenter, radius, weight])
+
+def circlepointmax(pos):
+	result = 0
+	for i in range(len(circles)):
+		thiscircle = circles[i]
+		dist = 0
+		for j in range(2):
+			dist += (pos[j] - thiscircle[j])**2
+		dist = np.sqrt(dist)
+		if dist < thiscircle[2] and thiscircle[3]>result:
+			result = thiscircle[3]
+	return result
+
+def circlemax(pos, width, height):
+	ncheck = 4
+	result = 0
+	for i in range(ncheck):
+		for j in range(ncheck):
+			checkx = pos[0] + i*width/ncheck
+			checky = pos[1] + j*width/ncheck
+			checkpos = [checkx, checky]
+			weight = circlepointmax(checkpos) 
+			if weight > result:
+				result = weight 
+	return result
+
+def maxfunc(pos, width, height):
+	return (numlevels+1)*(pos[0]/treewidth/2+pos[1]/treewidth/2)
+
+####################################################
+
+myimage = Image.open('name.png').convert('LA')
+flatlist = list(myimage.getdata())
+(imwidth, imheight) = myimage.size
+def invertcolor(ba):
+	if ba[1] == 0:
+		return 0 
+	else:
+		return 255 - ba[0]
+
+bwvalues = [[invertcolor(flatlist[imwidth*i + j]) for j in range(imwidth)] for i in range(imheight)]
+plt.imshow(bwvalues, cmap = plt.cm.gray)
+plt.show()
+
+treewidth = imwidth
+treeheight = imheight
+
+def picmax(pos, width, height):
+	ncheck = 1
+	result = 0
+	nvalues = 0
+	x0 = int(pos[0])
+	x1 = min(x0 + width, imwidth)
+	x1 = int(x1)
+	y0 = int(pos[1])
+	y1 = min(y0 + height, imheight)
+	y1 = int(y1)
+	for i in range(x0, x1, ncheck):
+		for j in range(y0, y1, ncheck):
+			result += bwvalues[j][i]
+			nvalues += 1
+	if nvalues > 0:
+		result /= nvalues
+	else:
+		result = 0
+	result /= 255
+	offset = 2
+	result = offset + result* (numlevels - offset+1)
+	return result
+
 squarelevels = numlevels
-squaretree = hd.HilbertSquare3(initsymmetry, 0) 
+squaretree = hd.HilbertTree(initsymmetry, 0) 
 squaretree.generatechildren(numlevels)
 positions = []
-squaretree.generatepositions(positions, [0,0], 2**numlevels)
-print(positions)
-
-#initconfig = hd.HilbertConfig(hd.LOWLEFTSTART, hd.COUNTERCLOCKWISE, [0,0], [0,0], squarelevels)
-#squaretree = hd.HilbertSquare(None, initconfig)
-#squaretree.generatechildren(squarelevels)
-#squaretree.generatewithmin(squarelevels, floorfunc2)
-#positions = []
-#squaretree.getpositionswithmin(positions)
-#squaretree.getpositions(positions)
-#
-#level = 1 
-#logwidth = power-1 
-#initsymmetry = hd.SquareSymmetry(0,0)
-#initposition = [0,0]
-#backwards = 0
-#squaretree2 = hd.HilbertSquare2(initsymmetry, initposition, logwidth, level, backwards)
-#squaretree2.generatechildren(level)
-#positions = []
-#squaretree2.getpositions(positions)
-
-print("Hilbert Curve generation done")
-
-#squaretree = hd.FourSquares(None, hd.LOWLEFTSTART, hd.COUNTERCLOCKWISE)
-
-#hd.generatesquaretree(squaretree, 4)
-#positions = []
-#initialpos = [0,0]
-#hd.generatepositions(positions, initialpos, squaretree, 4)
+squaretree.generatepositionswithmax(positions, [0,0], treewidth, treeheight, picmax)
 
 xpoints = [positions[i][0] for i in range(len(positions))]
-ypoints = [positions[i][1] for i in range(len(positions))]
+ypoints = [treeheight-positions[i][1] for i in range(len(positions))]
 
 fig = plt.figure(figsize = (2*8.6,2*3.2), dpi = 100, frameon = False)
 plt.plot(xpoints, ypoints, color = "blue")
-plt.axis([-1, 2**squarelevels, -1, 2**squarelevels])
+plt.axis([-1, treewidth+1, -1, treeheight+1])
 ax = plt.gca()
 ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
