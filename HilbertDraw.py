75b8a02f00135f6c1aa7c68084f265d222e3a50a
Matthew McGonagle <mwmcgonagle@gmail.com>
2016-12-31 19:52:06 -0500

    Created LevelFilter class. Moved different maximizing filter functions to classes inheriting LevelFilter class.

--- HilbertDraw.py
+++ HilbertDraw.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class SquareSymmetry:
 
@@ -76,17 +77,224 @@ class HilbertTree:
 				newposition[j] += offsets[newi][j]
 			self.children[i].generatepositions(currentlist, newposition, newwidth) 
 
-	def generatepositionswithmax(self, currentlist, myposition, mywidth, myheight, maxfunc):
-		if (not self.children) or self.level > maxfunc(myposition, mywidth, myheight):  
-			currentlist.append(myposition)
+class HilbertTreeMaxed(HilbertTree):
+
+	def __init__(self, symmetry, level, position, width, height, maxfunc):
+		super().__init__(symmetry, level)
+		self.position = position
+		self.width = width
+		self.height = height
+		self.maxfunc = maxfunc
+
+	def generatechildren(self, numlevels):
+		if self.level > numlevels or self.level > self.maxfunc(self.position, self.width, self.height):
 			return
-		newwidth = mywidth/2
-		newheight = myheight/2
+		elif self.children:
+			return
+
+		newwidth = self.width/2.0
+		newheight = self.height/2.0
 		offsets = [[0,0], [0,newheight], [newwidth,newheight], [newwidth,0]]
+		newpositions = [self.position.copy() for i in range(4)]
 		for i in range(4):
 			newi = self.symmetry.actindex(i)
-			newposition = myposition.copy()
 			for j in range(2):
-				newposition[j] += offsets[newi][j]
-			self.children[i].generatepositionswithmax(currentlist, newposition, newwidth, newheight, maxfunc) 
+				newpositions[i][j] += offsets[newi][j]
+
+		newlevel = self.level + 1
+		newsymmetries = [self.symmetry for i in range(4)]
+		newsymmetries[0] = self.symmetry.times(SquareSymmetry(3,1))
+		newsymmetries[3] = self.symmetry.times(SquareSymmetry(1,1))
+		for i in range(4):
+			self.children.append(HilbertTreeMaxed(newsymmetries[i], newlevel, newpositions[i], newwidth, newheight, self.maxfunc))
+		
+		for i in range(4):
+			self.children[i].generatechildren(numlevels)
+		
+	def generatepositions(self, currentlist):
+		if (not self.children):
+			currentlist.append(self.position)
+			return
+
+		for i in range(4):
+			self.children[i].generatepositions(currentlist)
+
+
+class ImageProcessing:
+
+	def invertcolor(ba):
+		if ba[1] == 0:
+			return 0 
+		else:
+			return 255 - ba[0]
+	
+	def bwtolevels(bw, minlevel, maxlevel):
+		base = 2.5
+		maxbw = 0
+		for i in range(len(bw)):
+			testmax = max(bw[i])
+			if testmax > maxbw:
+				maxbw = testmax
+	
+		if maxbw == 0:
+			maxbw = 1.0
+	
+		for i in range(len(bw)):
+			for j in range(len(bw[0])):
+				level = bw[i][j]/maxbw
+				level *= base**maxlevel - base**minlevel 
+				level += base**minlevel 
+				level = np.log(level)/np.log(base)
+				bw[i][j] = level
+
+class LevelFilter:
+
+	def __init__(self, levels):
+		self.levels = levels
+		self.imwidth = len(levels[0])
+		self.imheight = len(levels)
+		self.x0 = 0
+		self.x1 = self.imwidth
+		self.y0 = 0
+		self.y1 = self.imheight
+
+	def filterfunc(pos, width, height):
+		return 0
+
+	def setupxy(self, pos, width, height):
+		self.x0 = int(pos[0])
+		self.x0 = max(self.x0, 0)
+		self.x1 = int(self.x0 + width)
+		self.x1 = min(self.x1, self.imwidth)
+
+		self.y0 = int(pos[1])
+		self.y0 = max(self.y0, 0)
+		self.y1 = int(self.y0 + height)
+		self.y1 = min(self.y1, self.imheight)
+
+class UseMax(LevelFilter):
+
+	def filterfunc(self, pos, width, height):
+		floorlevel = 0
+		levelsmax = 0
+		self.setupxy(pos, width, height)
+		
+		for i in range(self.x0, self.x1, 1):
+			for j in range(self.y0, self.y1, 1):
+				if self.levels[j][i] > levelsmax:
+					levelsmax = self.levels[j][i]
+		if levelsmax < floorlevel:
+			return floorlevel
+		else:
+			return levelsmax 
+
+class UseAverage(LevelFilter):
+
+	def filterfunc(self, pos, width, height):
+		dx = 1
+		dy = 1
+		average = 0
+		nvalues = 0
+		self.setupxy(pos, width, height)
+
+		for i in range(self.x0, self.x1, dx):
+			for j in range(self.y0, self.y1, dy):
+				average += self.levels[j][i]
+				nvalues += 1
+		if nvalues > 0:
+			average /= nvalues
+		else:
+			average = 0
+
+		return average 
+
+class UseMajority(LevelFilter):
+
+	def __init__(self, levels, numlevels):
+		super().__init__(levels)
+		self.numlevels = numlevels
+
+	def filterfunc(self, pos, width, height):
+		floorpercent = 0.99
+		minreturn = 0 # numlevels-3 
+	
+		dx = 1
+		dy = 1
+		self.setupxy(pos, width, height)
+		
+		frequency = [0 for i in range(self.numlevels+1)]
+		nvalues = 0
+		for i in range(self.x0, self.x1, dx):
+			for j in range(self.y0, self.y1, dy):
+				k = self.levels[j][i]
+				k = max(0.0, k)
+				k = int(k)
+				k = min(len(frequency), k)
+				frequency[k] += 1
+				nvalues += 1
+		result = 0
+		maxfrequency = 0
+		someabovefloor = False
+		floor = nvalues * floorpercent
+		for i in range(len(frequency)):
+			if frequency[i] >= maxfrequency:
+				maxfrequency = frequency[i]
+				result = i
+			if frequency[i] > floor:
+				someabovefloor = True
+		if someabovefloor:
+			if result > minreturn:
+				return result
+			else:
+				return minreturn
+		else:
+			return self.numlevels+1
+
+class CircleFilter(LevelFilter):
+
+	def __init__(self, levels, maxlevel, treewidth, treeheight):
+
+		super().__init__(levels)
+
+		maxradius = np.sqrt(treewidth**2 + treeheight**2) / 3.0
+		floor = 3
+		self.circles = []
+		for i in range(15):
+			radius = random.random()* maxradius
+			xcenter = random.random()*treewidth
+			ycenter = random.random()*treeheight
+			circlelevel = maxlevel - floor + random.random() * floor
+			self.circles.append([xcenter, ycenter, radius, circlelevel])
+	
+	def circlepointmax(self, pos):
+		result = 0
+		for i in range(len(self.circles)):
+			thiscircle = self.circles[i]
+			dist = 0.0
+			for j in range(2):
+				dist += (pos[j] - thiscircle[j])**2
+			dist = np.sqrt(dist)
+			if dist < thiscircle[2] and thiscircle[3]>result:
+				result = thiscircle[3]
+		return result
+	
+	def filterfunc(self, pos, width, height):
+		ncheck = 5
+		result = 0
+		dx = width / ncheck	
+		dy =  height / ncheck 
+
+		checkx = pos[0]
+		checky = pos[1]
+		for i in range(ncheck):
+			for j in range(ncheck):
+				checkpos = [checkx, checky]
+				circlelevel = self.circlepointmax(checkpos) 
+				if circlelevel > result:
+					result = circlelevel 
+				checky += dy
+			checkx += dx
+
+		return result
+
 
--- main.py
+++ main.py
@@ -6,100 +6,42 @@ import scipy
 import random 
 from PIL import Image
 import PIL.ImageOps
+import cProfile
 
 initsymmetry = hd.SquareSymmetry(0,0)
 initlevel = 0
 numlevels = 7
 treewidth = 2**numlevels
 
-circles = []
-for i in range(15):
-	radius = random.random()*treewidth/3
-	xcenter = random.random()*treewidth
-	ycenter = random.random()*treewidth
-	weight = numlevels - 3 + random.random()*4
-	circles.append([xcenter, ycenter, radius, weight])
-
-def circlepointmax(pos):
-	result = 0
-	for i in range(len(circles)):
-		thiscircle = circles[i]
-		dist = 0
-		for j in range(2):
-			dist += (pos[j] - thiscircle[j])**2
-		dist = np.sqrt(dist)
-		if dist < thiscircle[2] and thiscircle[3]>result:
-			result = thiscircle[3]
-	return result
-
-def circlemax(pos, width, height):
-	ncheck = 4
-	result = 0
-	for i in range(ncheck):
-		for j in range(ncheck):
-			checkx = pos[0] + i*width/ncheck
-			checky = pos[1] + j*width/ncheck
-			checkpos = [checkx, checky]
-			weight = circlepointmax(checkpos) 
-			if weight > result:
-				result = weight 
-	return result
-
-def maxfunc(pos, width, height):
-	return (numlevels+1)*(pos[0]/treewidth/2+pos[1]/treewidth/2)
-
-####################################################
-
-myimage = Image.open('name.png').convert('LA')
+myimage = Image.open('mario.png').convert('LA')
 flatlist = list(myimage.getdata())
 (imwidth, imheight) = myimage.size
-def invertcolor(ba):
-	if ba[1] == 0:
-		return 0 
-	else:
-		return 255 - ba[0]
-
-bwvalues = [[invertcolor(flatlist[imwidth*i + j]) for j in range(imwidth)] for i in range(imheight)]
-plt.imshow(bwvalues, cmap = plt.cm.gray)
-plt.show()
 
 treewidth = imwidth
 treeheight = imheight
+bwvalues = [ [hd.ImageProcessing.invertcolor(flatlist[imwidth*i + j]) for j in range(imwidth)] 
+		for i in range(imheight)]
+hd.ImageProcessing.bwtolevels(bwvalues, 0, numlevels)
 
-def picmax(pos, width, height):
-	ncheck = 1
-	result = 0
-	nvalues = 0
-	x0 = int(pos[0])
-	x1 = min(x0 + width, imwidth)
-	x1 = int(x1)
-	y0 = int(pos[1])
-	y1 = min(y0 + height, imheight)
-	y1 = int(y1)
-	for i in range(x0, x1, ncheck):
-		for j in range(y0, y1, ncheck):
-			result += bwvalues[j][i]
-			nvalues += 1
-	if nvalues > 0:
-		result /= nvalues
-	else:
-		result = 0
-	result /= 255
-	offset = 2
-	result = offset + result* (numlevels - offset+1)
-	return result
+#myfilter = hd.UseMax(bwvalues)
+#myfilter = hd.CircleFilter(bwvalues, numlevels, treewidth, treeheight)
+myfilter = hd.UseMajority(bwvalues, numlevels)
 
-squarelevels = numlevels
-squaretree = hd.HilbertTree(initsymmetry, 0) 
+plt.imshow(bwvalues, cmap = plt.cm.gray)
+plt.show()
+
+squaretree = hd.HilbertTreeMaxed(initsymmetry, 0, [0,0], treewidth, treeheight, myfilter.filterfunc) 
 squaretree.generatechildren(numlevels)
+
 positions = []
-squaretree.generatepositionswithmax(positions, [0,0], treewidth, treeheight, picmax)
+squaretree.generatepositions(positions)
 
 xpoints = [positions[i][0] for i in range(len(positions))]
 ypoints = [treeheight-positions[i][1] for i in range(len(positions))]
 
 fig = plt.figure(figsize = (2*8.6,2*3.2), dpi = 100, frameon = False)
 plt.plot(xpoints, ypoints, color = "blue")
+
 plt.axis([-1, treewidth+1, -1, treeheight+1])
 ax = plt.gca()
 ax.axes.get_xaxis().set_visible(False)
@@ -110,6 +52,6 @@ ax.spines['bottom'].set_visible(False)
 ax.spines['left'].set_visible(False)
 
 
-plt.savefig("Output.png", bbox_inches = "tight", dpi = 100)
+plt.savefig("Output.png", bbox_inches = "tight", dpi = 300)
 plt.show()
 
